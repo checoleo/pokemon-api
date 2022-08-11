889b938c875893bd2ada6a84d3ed78184e1f2bf1
checoleo <sergio.arteaga@codetidy.com.mx>
2022-08-11 12:50:28 -0500

    ajuste para el disable button del pokeform

--- src/components/PokemonApp.jsx
+++ src/components/PokemonApp.jsx
@@ -10,7 +10,7 @@ const useStyles = () => ({
   mainContainer: {
     margin: "auto",
     width: "50%",
-    border: "3px solid black",
+    boxShadow:"0px 3px 3px -2px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 1px 8px 0px rgba(0,0,0,0.12)",
     padding: "30px"
   },
   topContainer: {
